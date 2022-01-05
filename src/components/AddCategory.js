4594d4ceda548d5969d3027e2a0fb8d834036628
Daniel Alexander Carrasco Cunza <dacc.carrasco@gmail.com>
2022-01-05 15:06:41 -0500

    input modificado

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -12,7 +12,7 @@ export const AddCategory = ({ setCategories }) => {
     const handleSubmit = (e) =>{
         e.preventDefault();
         if (inputValue.length > 2) {
-            setCategories( cats => [ inputValue,...cats]);
+            setCategories( cats => [ inputValue, ...cats ]);
             setInputValue('');
         }
            
